7599fb5d4ccbfb4cec7ec0fcc783d87e28e6f7c6
tramcctw <250438930@qq.com>
2020-04-16 19:00:36 +0800

    数据更新

---
+++ src/views/home/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="home">
+    <nav-bar class="hoem-nav">
+      <div slot="center">购物街</div>
+    </nav-bar>
+  </div>
+</template>
+
+<script>
+  import NavBar from "components/common/navbar/NavBar";
+  export default {
+    name: "Home",
+    components:{
+      NavBar
+    }
+  }
+</script>
+
+<style scoped>
+.hoem-nav{
+  background-color: var(--color-tint);
+  color: #fff;
+}
+</style>

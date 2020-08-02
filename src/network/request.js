28133ff6d7711de07385d509ac8587b394e4f380
tramcctw <tramcctw@foxmail.com>
2020-08-02 21:21:26 +0800

    提交数据

--- src/network/request.js
+++ src/network/request.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 export function request(config){
   const instance = axios.create({
-    baseURL:'http://152.136.185.210:8000/api/z8',
+    baseURL:'接口找老师要',
     timeout:5000
   })
 
